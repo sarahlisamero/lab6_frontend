370c5e55afcc8ca22975edde77f08e8265ecf245
Sarah-Lisa Mero <sarahlisamero@gmail.com>
2023-11-27 20:20:24 +0100

    feat: add basic frontend layout

---
+++ src/components/Ranking.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+
+let selectedTeam = ref('');
+let score = ref(0);
+let teams = ref([
+  { name: 'De Badeendjes', score: 0 },
+  { name: 'De Holle Bolle Gijsjes', score: 0 },
+  { name: 'De Koele Kikkers', score: 0 },
+  { name: 'De Zwemmende Zwanen', score: 0 },
+]);
+
+</script>
+
+<template>
+  <div>
+    <ul>
+      <li v-for="team in teams" :key="team.name">
+        {{ team.name }} - {{ team.score }}
+      </li>
+    </ul>
+    <div>
+      <label for="team">Select Team:</label>
+      <select v-model="selectedTeam">
+        <option v-for="team in teams" :key="team.name" :value="team.name">
+          {{ team.name }}
+        </option>
+      </select>
+      <label for="score">Enter Score:</label>
+      <input v-model="score" type="number" />
+      <button @click="updateStatistics">Update Statistics</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
